71670091aebcae677e16525cb5586aa2170e9a08
AlexandruTajamar21 <alexandru.preutesei@tajamar365.com>
2022-01-21 14:15:16 +0100

    Add project files.

---
+++ ProyectoTiendaMagic/Controllers/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoTiendaMagic.Data;
+using ProyectoTiendaMagic.Models;
+using ProyectoTiendaMagic.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoTiendaMagic.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private IRepositoryUsuarios repo;
+
+        public UsuarioController(IRepositoryUsuarios repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Registro()
+        {
+            return View();
+        }
+    }
+}
